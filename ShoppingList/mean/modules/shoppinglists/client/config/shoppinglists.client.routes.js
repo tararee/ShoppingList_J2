051f2d56f7e0a6dd3a4bc4446980da48221b0881
Matthew Kory <matthewskory@gmail.com>
2016-09-15 09:36:30 -0400

    cleaned up formatting and added checkbox, still need to create css for strikethrough

--- ShoppingList/mean/modules/shoppinglists/client/config/shoppinglists.client.routes.js
+++ ShoppingList/mean/modules/shoppinglists/client/config/shoppinglists.client.routes.js
@@ -1,79 +1,79 @@
-(function () {
-  'use strict';
+// (function () {
+//   'use strict';
 
-  angular
-    .module('shoppinglists')
-    .config(routeConfig);
+//   angular
+//     .module('shoppinglists')
+//     .config(routeConfig);
 
-  routeConfig.$inject = ['$stateProvider'];
+//   routeConfig.$inject = ['$stateProvider'];
 
-  function routeConfig($stateProvider) {
-    $stateProvider
-      .state('shoppinglists', {
-        abstract: true,
-        url: '/shoppinglists',
-        template: '<ui-view/>'
-      })
-      .state('shoppinglists.list', {
-        url: '',
-        templateUrl: 'modules/shoppinglists/client/views/list-shoppinglists.client.view.html',
-        controller: 'ShoppinglistsListController',
-        controllerAs: 'vm',
-        data: {
-          pageTitle: 'Shoppinglists List'
-        }
-      })
-      .state('shoppinglists.create', {
-        url: '/create',
-        templateUrl: 'modules/shoppinglists/client/views/form-shoppinglist.client.view.html',
-        controller: 'ShoppinglistsController',
-        controllerAs: 'vm',
-        resolve: {
-          shoppinglistResolve: newShoppinglist
-        },
-        data: {
-          roles: ['user', 'admin'],
-          pageTitle : 'Shoppinglists Create'
-        }
-      })
-      .state('shoppinglists.edit', {
-        url: '/:shoppinglistId/edit',
-        templateUrl: 'modules/shoppinglists/client/views/form-shoppinglist.client.view.html',
-        controller: 'ShoppinglistsController',
-        controllerAs: 'vm',
-        resolve: {
-          shoppinglistResolve: getShoppinglist
-        },
-        data: {
-          roles: ['user', 'admin'],
-          pageTitle: 'Edit Shoppinglist {{ shoppinglistResolve.name }}'
-        }
-      })
-      .state('shoppinglists.view', {
-        url: '/:shoppinglistId',
-        templateUrl: 'modules/shoppinglists/client/views/view-shoppinglist.client.view.html',
-        controller: 'ShoppinglistsController',
-        controllerAs: 'vm',
-        resolve: {
-          shoppinglistResolve: getShoppinglist
-        },
-        data:{
-          pageTitle: 'Shoppinglist {{ articleResolve.name }}'
-        }
-      });
-  }
+//   function routeConfig($stateProvider) {
+//     $stateProvider
+//       .state('shoppinglists', {
+//         abstract: true,
+//         url: '/shoppinglists',
+//         template: '<ui-view/>'
+//       })
+//       .state('shoppinglists.list', {
+//         url: '',
+//         templateUrl: 'modules/shoppinglists/client/views/list-shoppinglists.client.view.html',
+//         controller: 'ShoppinglistsListController',
+//         controllerAs: 'vm',
+//         data: {
+//           pageTitle: 'Shoppinglists List'
+//         }
+//       })
+//       .state('shoppinglists.create', {
+//         url: '/create',
+//         templateUrl: 'modules/shoppinglists/client/views/form-shoppinglist.client.view.html',
+//         controller: 'ShoppinglistsController',
+//         controllerAs: 'vm',
+//         resolve: {
+//           shoppinglistResolve: newShoppinglist
+//         },
+//         data: {
+//           roles: ['user', 'admin'],
+//           pageTitle : 'Shoppinglists Create'
+//         }
+//       })
+//       .state('shoppinglists.edit', {
+//         url: '/:shoppinglistId/edit',
+//         templateUrl: 'modules/shoppinglists/client/views/form-shoppinglist.client.view.html',
+//         controller: 'ShoppinglistsController',
+//         controllerAs: 'vm',
+//         resolve: {
+//           shoppinglistResolve: getShoppinglist
+//         },
+//         data: {
+//           roles: ['user', 'admin'],
+//           pageTitle: 'Edit Shoppinglist {{ shoppinglistResolve.name }}'
+//         }
+//       })
+//       .state('shoppinglists.view', {
+//         url: '/:shoppinglistId',
+//         templateUrl: 'modules/shoppinglists/client/views/view-shoppinglist.client.view.html',
+//         controller: 'ShoppinglistsController',
+//         controllerAs: 'vm',
+//         resolve: {
+//           shoppinglistResolve: getShoppinglist
+//         },
+//         data:{
+//           pageTitle: 'Shoppinglist {{ articleResolve.name }}'
+//         }
+//       });
+//   }
 
-  getShoppinglist.$inject = ['$stateParams', 'ShoppinglistsService'];
+//   getShoppinglist.$inject = ['$stateParams', 'ShoppinglistsService'];
 
-  function getShoppinglist($stateParams, ShoppinglistsService) {
-    return ShoppinglistsService.get({
-      shoppinglistId: $stateParams.shoppinglistId
-    }).$promise;
-  }
+//   function getShoppinglist($stateParams, ShoppinglistsService) {
+//     return ShoppinglistsService.get({
+//       shoppinglistId: $stateParams.shoppinglistId
+//     }).$promise;
+//   }
 
-  newShoppinglist.$inject = ['ShoppinglistsService'];
+//   newShoppinglist.$inject = ['ShoppinglistsService'];
 
-  function newShoppinglist(ShoppinglistsService) {
-    return new ShoppinglistsService();
-  }
-})();
+//   function newShoppinglist(ShoppinglistsService) {
+//     return new ShoppinglistsService();
+//   }
+// })();
